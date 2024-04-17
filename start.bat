8421124b8819fa269aa153218198dff9671c208d
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-04-18 01:22:59 +0530

    Added start.bat

---
+++ start.bat
@@ -0,0 +1,9 @@
+@echo off
+
+rem Navigate to the server directory and start the server
+cd /d "C:\Users\LENOVO\Desktop\New folder\server"
+start npm run dev
+
+rem Navigate to the client directory and start the frontend
+cd /d "C:\Users\LENOVO\Desktop\New folder\client"
+start npm run start
