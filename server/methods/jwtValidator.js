7e1b8a8507f5492349df165f26ba0d2f6ff073d1
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-04-19 22:05:46 +0530

    JWT Token comments

--- server/methods/jwtValidator.js
+++ server/methods/jwtValidator.js
@@ -4,6 +4,9 @@ const SECRET_KEY = 'vgvsvTGusrlx2jdZi7oVVDQBtTjlOO6j';
 
 
 class JSON_WEB_TOKEN{
+
+    // Init payload of the jwt token
+
     createPayload(userId,username,email){
         return {
             userId: userId,
@@ -13,16 +16,23 @@ class JSON_WEB_TOKEN{
     }
 
     createToken(payload){
+        // return the signed jwt token
         return jwt.sign(payload, SECRET_KEY, { expiresIn: '7d' })
     }
 
     validateUserToken(userToken){
         try {
-            const decodedToken = jwt.verify(userToken, SECRET_KEY);
-            console.log(decodedToken);
-            if (decodedToken.exp < Math.floor(Date.now() / 1000)) {
-                return { valid: false, reason: 'Token has expired' };
+            const decodedToken = jwt.verify(userToken, SECRET_KEY);     // fetch the decoded token using the secrete key and userToken
+            // console.log(decodedToken);
+            
+            // If token is not verified, then it will throw an error
+
+            if (decodedToken.exp < Math.floor(Date.now() / 1000)) {     // check if the token has expired
+                return { valid: false, reason: 'Token has expired' };   
             }
+
+            // return valid token
+
             return { valid: true, decodedToken: decodedToken };
         } catch (error) {
             return { valid: false, reason: 'Invalid token: ' + error.message };
